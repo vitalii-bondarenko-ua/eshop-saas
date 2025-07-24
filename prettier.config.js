04274a135a1a27ccc023ebc51e464707aa0fd674
Vitalii.B <vitalii.bondarenko.ua@gmail.com>
2025-07-24 13:24:24 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/4C9zbLIREl

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+{
+  printWidth: 80,
+      tabWidth: 4,
+      singleQuote: true,
+      trailingComma: 'es5',
+      overrides: [
+          {
+              files: '**/*.json',
+              options: {
+                  tabWidth: 2,
+              },
+          },
+      ],
+}
