db2aa302f91932886258b0c630659f93599723a9
Vitalii.B <vitalii.bondarenko.ua@gmail.com>
2025-07-24 13:49:36 +0300

    update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,14 @@
-{
-  printWidth: 80,
-      tabWidth: 4,
-      singleQuote: true,
-      trailingComma: 'es5',
-      overrides: [
-          {
-              files: '**/*.json',
-              options: {
-                  tabWidth: 2,
-              },
-          },
-      ],
-}
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5',
+    overrides: [
+        {
+            files: '**/*.json',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
